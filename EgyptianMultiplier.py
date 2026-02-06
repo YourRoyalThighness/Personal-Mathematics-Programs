4a9d8b3cc7cf52d69298b453f7681ecb8e570fa5
YourRoyalThighness <doodlebtw@gmail.com>
2026-02-06 14:39:33 -0800

    Update EgyptianMultiplier.py
    fixed negative issues,
    answer is correct however, visually, the numbers are incorrect because of how i handled the integer inputs

--- EgyptianMultiplier.py
+++ EgyptianMultiplier.py
@@ -10,11 +10,22 @@ Code Review:
 import math
 
 # user input, a is left side being doubled, b is right side being cut in half
-a = int(input("First number: "))
-b = int(input("Second number: "))
-print("")
+a = 0
+b = 0
 
-# stores original values for last print statement
+# ensures program does not continue until integer is inputted
+while True:  
+    try:
+        a = int(input("First integer: "))
+        b = int(input("Second integer: "))
+        break
+    except ValueError:
+        print("Please put in an integer ")
+
+
+
+
+# copies original values for final print statement
 x = a
 y = b
 
@@ -22,15 +33,20 @@ y = b
 print(a * b)
 print("")
 
-# fixes things if the second number is negative
-if b < 0:
+# if statements to fix things with negative numbers
+if b < 0 and a > 0:
     a *= -1
+    b = int(math.fabs(b))
+if b < 0 and a < 0:
+    a = int(math.fabs(a))
+    b = int(math.fabs(b))
 
 # Loop to run numbers through, repeatedly multipliying one number and dividing the other
 sum_List = []
 
+
 # arbitrary for loop range to ensure complete multiplication
-for i in range(100):
+for i in range(1000000):
 
     # checks if algorithm has completed
     if b == 1 or b == -1:
@@ -42,10 +58,12 @@ for i in range(100):
     if b % 2 == 1:
         sum_List.append(a)
 
-    # computational section, outputs old a and b, then updates values
+    # computational section, outputs old a and b into "table", then updates values for next iteration
     print(f"{a:12.0f} : {b}")
     a *= 2
+    
     b = b // 2
+    
 
 # prints answer
 print("")
